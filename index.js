01e1de5a30a9ab9c85bbc9cf3eb6aa8bd15c8cae
Ulises Hernandez <uahm93@gmail.com>
2021-06-23 16:49:21 -0500

    First module basic maths

---
+++ index.js
@@ -0,0 +1,66 @@
+module.exports = { 
+
+    /**
+     * Sum of two numbers
+     * 
+     * @example 
+     * 
+     * n1 = 1, n2 = 3 => result 4
+     * 
+     * @param {*} n1 First digit 
+     * 
+     * @param {*} n2 Second digit
+     * 
+     * @returns 
+     */
+    sum: function(n1, n2){
+        return n1+n2;
+    },
+
+    /**
+     * subtraction of two numbers
+     * @example 
+     * n1 = 2, n2 = 1 => result 1
+     * 
+     * @param {*} n1 First digit 
+     * 
+     * @param {*} n2 Second digit
+     * 
+     * @returns 
+     */
+    subtraction: function(n1, n2){
+        return n1-n2;
+    },
+
+    /**
+     * Multiplication of two numbers
+     * @example 
+     * n1 = 1, n2 = 3 => result 3
+     * 
+     * @param {*} n1 First digit 
+     * 
+     * @param {*} n2 Second digit
+     * 
+     * @returns 
+     */
+    multi: function (n1, n2) {
+        return n1*n2;
+    },
+
+
+    /**
+     * Divition two numbers
+     * @example 
+     * n1 = 1, n2 = 3 => result 3
+     * 
+     * @param {*} n1 First digit 
+     * 
+     * @param {*} n2 Second digit
+     * 
+     * @returns 
+     */
+    divition: function (n1, n2) {
+        return n1/n2;
+    }
+
+ }
---
+++ tests.js
@@ -0,0 +1,6 @@
+const m = require('.');
+
+console.log(m.divition(123,1));
+console.log(m.multi(123,12));
+console.log(m.sum(123,1));
+console.log(m.subtraction(123,1));
